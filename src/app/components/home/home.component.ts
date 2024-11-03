d21caf5458aa04c548a50d600e3492f60c1fe07c
Van Ortega <ortegavan@live.com>
2024-11-03 18:18:33 -0300

    feat: contato

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { AboutBookComponent } from '../about-book/about-book.component';
 import { WhoComponent } from '../who/who.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeroComponent, AboutBookComponent, WhoComponent],
+    imports: [
+        HeroComponent,
+        AboutBookComponent,
+        WhoComponent,
+        ContactComponent,
+    ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
