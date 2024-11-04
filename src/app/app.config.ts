3e4eecd05267394f84a254e33f36fae02e98ff0f
Van Ortega <ortegavan@live.com>
2024-11-03 21:08:23 -0300

    chore: hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import {
+    provideRouter,
+    withHashLocation,
+    withInMemoryScrolling,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -8,6 +12,7 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(
             routes,
+            withHashLocation(),
             withInMemoryScrolling({
                 anchorScrolling: 'enabled',
                 scrollPositionRestoration: 'enabled',
