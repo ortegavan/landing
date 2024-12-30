3f351dbca4ef4a4496a9b57b7c8189acd1575acd
Van Ortega <ortegavan@live.com>
2024-12-29 22:00:42 -0300

    chore: remove primeng 17

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-hero',
     standalone: true,
-    imports: [ButtonModule, RouterLink],
+    imports: [RouterLink],
     templateUrl: './hero.component.html',
     styleUrl: './hero.component.css',
 })
