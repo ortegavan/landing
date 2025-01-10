bfa5ddeae46c8b9d9c8f76142c5a4838f770fef5
Vanessa Ortega <ortegavan@live.com>
2025-01-10 01:06:12 -0300

    feat: aplica link de compra

--- src/app/components/price/price.component.ts
+++ src/app/components/price/price.component.ts
@@ -8,20 +8,6 @@ import { Component } from '@angular/core';
 })
 export class PriceComponent {
     comprarKindle() {
-        window.open(
-            'https://www.amazon.com.br/Livros/b/?ie=UTF8&node=6740748011&ref_=nav_cs_books',
-            '_blank'
-        );
-    }
-
-    comprarBrochura() {
-        throw new Error('Not implemented yet');
-    }
-
-    comprarCapaDura() {
-        window.open(
-            'https://www.amazon.com.br/kindle-dbs/storefront?storeType=browse&node=5475882011&ref_=nav_cs_kindle_books',
-            '_blank'
-        );
+        window.open('https://a.co/d/aaHQg69', '_blank');
     }
 }
