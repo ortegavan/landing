a4d8f1c0e7f346695ab4c6b18752af8410439e93
Van Ortega <ortegavan@live.com>
2025-12-21 16:27:21 -0300

    chore: otimiza scores do lighthouse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./components/home/home.component').then(
+                (m) => m.HomeComponent,
+            ),
+    },
     {
         path: 'imagens',
         loadComponent: () =>
